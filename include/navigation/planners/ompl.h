7d6c75b76193726d5f01295c9d1e11f76d95e0ee
kvmanohar22 <kvmanohar22@gmail.com>
2017-06-05 01:14:21 +0530

    Adding ompl layout

---
+++ include/navigation/planners/ompl.h
@@ -0,0 +1,37 @@
+#ifndef _OMPL_MOTION_PLANNER_H_
+#define _OMPL_MOTION_PLANNER_H_
+#include <ompl/geometric/SimpleSetup.h>
+
+#include <ompl/geometric/planners/prm/PRM.h>
+#include <ompl/geometric/planners/rrt/RRTstar.h>
+#include <ompl/geometric/planners/rrt/LBTRRT.h>
+#include <ompl/geometric/planners/rrt/LazyRRT.h>
+#include <ompl/geometric/planners/rrt/RRT.h>
+#include <ompl/geometric/planners/rrt/RRTConnect.h>
+#include <ompl/geometric/planners/rrt/TRRT.h>
+#include <ompl/geometric/planners/rrt/pRRT.h>
+#include <ompl/geometric/planners/est/EST.h>
+
+#include "ompl/base/StateSpace.h"
+#include "ompl/base/spaces/RealVectorStateSpace.h"
+#include "ompl/base/spaces/SO2StateSpace.h"
+
+#include <ompl/base/spaces/SE2StateSpace.h>
+#include <ompl/base/PlannerData.h>
+#include <cmath>
+#include <iostream>
+#include <fstream>
+#include <ostream>
+#include <vector>
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/opencv.hpp>
+
+
+namespace ob = ompl::base;
+namespace og = ompl::geometric;
+
+using namespace std;
+
+#endif
